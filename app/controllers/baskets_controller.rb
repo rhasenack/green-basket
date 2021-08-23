68bc192641146f2a26ee065684f979a1a9768b2d
Ricardo Hasenack <rhasenack@gmail.com>
2021-08-23 16:29:02 -0300

    basic routes - list all baskets, got to basket page, let logged user claim a basket'

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,4 +1,6 @@
 class BasketsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @baskets = Basket.all
   end
@@ -7,4 +9,24 @@ class BasketsController < ApplicationController
     @basket = Basket.find(params[:id])
   end
 
+  def new
+    @basket = Basket.new()
+  end
+
+  def create
+    @basket = Basket.new(basket_params)
+    # IF USER IS LOGGED IN
+    @basket.user_id = current_user.id
+    if @basket.save
+      redirect_to baskets_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def basket_params
+    params.require(:basket).permit(:description, :address, :price, :stock)
+  end
 end
---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+
+
+  def index
+    @orders = Order.all.where("user_id = #{current_user.id}")
+  end
+
+  def new
+    @basket = Basket.find(params[:basket_id])
+    @order = Order.new()
+  end
+
+  def create
+    basket = Basket.find(params[:basket_id])
+    @order = Order.new()
+    @order.price = basket.price
+    @order.status = 'requested'
+    @order.basket_id = basket.id
+    @order.user_id = current_user.id
+    @order.save
+    redirect_to
+  end
+
+end
