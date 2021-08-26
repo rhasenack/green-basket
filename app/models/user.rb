0a8d221e734a09933626948cd87348e31326117b
Ricardo Hasenack <rhasenack@gmail.com>
2021-08-26 15:08:30 -0300

    added stock control

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   ROLES = %w[Restaurant Consumer]
   validates :role, inclusion: { in: ROLES }
 
