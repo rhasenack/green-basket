16745ba291ddb302e04eda8b19fe8570c7759b83
Nathan Debaz <nathandebaz266@gmail.com>
2021-08-27 15:59:15 -0300

    correção com rafa

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -42,5 +42,16 @@ document.addEventListener('turbolinks:load', () => {
   initMapbox();
   loadDynamicBannerText();
 
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.body.style.backgroundColor = "white";
+  }
 
 });
